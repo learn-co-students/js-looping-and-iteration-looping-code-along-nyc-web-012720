227cf18b7c7b1cf365ec2f82d8d4e64d451ba286
Kelsbecker <kelly.lynn.becker@gmail.com>
2020-03-08 16:00:19 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(stringsArray, eventName) {
+    let thankYou = []
+    for(let i = 0; i < stringsArray.length; i ++) {
+        thankYou.push(`Thank you, ${stringsArray[i]}, for the wonderful ${eventName} gift!`);
+    }
+        return thankYou;
+}
+
+function countDown(integer) {
+    while(integer > 0) {
+        console.log(integer);
+        integer -= 1;
+    }
+    console.log(integer);
+}
+
