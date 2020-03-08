44f42c8d765e9be7fe80ca1e3b7b5163af237cb3
aprilfaithdays <aprilfaith.e@gmail.com>
2020-03-07 23:56:25 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(names, eventName) {
+    let thankYouArray = [];
+    for (let i = 0; i < names.length; i++) {
+        thankYouArray.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    };
+    return thankYouArray
+}
+
+function countDown(n) {
+    let i = n
+    while (i >= 0 ) {
+        console.log(i);
+        i--
+    };
+}
